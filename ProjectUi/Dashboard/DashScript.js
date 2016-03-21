bec0066b2f92308ab991b8a74a872eb8b2ceba3c
kochi007 <kaikobad.kaikobad326@gmail.com>
2016-03-21 22:32:18 +0600

    First

---
+++ ProjectUi/Dashboard/DashScript.js
@@ -0,0 +1,29 @@
+$.getJSON("G://ProjectUi/Dashboard/DashJSON.json",loadOptions)
+
+function loadOptions(data) {
+	console.log(data);
+	console.log(data.ManageRoute);
+	console.log(data.ManageBus);
+	console.log(data.BusTicketInfo);
+
+	$.each(data.ManageRoute, function(index, value) {		
+		var routes = $("<li>", {
+			"text" : value
+		});
+		routes.appendTo($("#routes"));
+	});
+
+	$.each(data.ManageBus, function(index, value) {		
+		var Bus = $("<li>", {
+			"text" : value
+		});
+		Bus.appendTo($("#Bus"));
+	});
+	$.each(data.BusTicketInfo, function(index, value) {		
+		var TicketInfo = $("<li>", {
+			"text" : value
+		});
+		TicketInfo.appendTo($("#TicketInfo"));
+
+	});
+}
